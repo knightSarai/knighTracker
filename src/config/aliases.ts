cab2a24e78846cd910df1fe344d36b3d4b1e2993
knight <knight.sarai.dev@gmail.com>
2022-01-22 21:22:04 +0200

    Setup the aliases

---
+++ src/config/aliases.ts
@@ -0,0 +1,11 @@
+export const aliases = (prefix = `src`) => ({
+  '@components': `${prefix}/components`,
+  '@config': `${prefix}/config`,
+  '@hooks': `${prefix}/hooks`,
+  '@pages': `${prefix}/pages`,
+  '@global': `${prefix}/global`,
+  '@styles': `${prefix}/styles`,
+  '@utils': `${prefix}/utils`,
+  '@models': `${prefix}/models`,
+  '@state': `${prefix}/state`,
+});
